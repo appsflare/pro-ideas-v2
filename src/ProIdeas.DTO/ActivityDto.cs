0a3c7f67b39d6cc7bddefd015dbbde80065249ef
Srinath Janakiraman <s.janakiraman@agilecockpti.com>
2017-05-11 22:45:44 +0530

    Created Activity Entities

---
+++ src/ProIdeas.DTO/ActivityDto.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ProIdeas.DTO
+{
+    public class ActivityDto : BaseDto<string>
+    {
+
+        public string Body { get; set; }
+
+        public string Type { get; set; }
+
+        public string ItemId { get; set; }
+
+        public string ItemOwnerId { get; set; }
+
+        public UserDto ItemOwner { get; set; }
+
+        public ActivityItemDetailsDto ItemDetails { get; set; }
+
+        public string OwnerId { get; set; }
+
+        public UserDto Owner { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+    }
+
+    public class ActivityItemDetailsDto
+    {
+        public string IdeaId { get; set; }
+
+        public string TargetOwnerId { get; set; }
+
+        public bool IsUpVote { get; set; }
+
+        public UserDto TargetOwner { get; set; }
+    }
+}
---
+++ src/ProIdeas.Domain.Entities/Activity.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ProIdeas.Domain.Entities
+{
+    public class Activity : BaseEntity
+    {
+
+        public string Body { get; set; }
+
+        public string Type { get; set; }
+
+        public string ItemId { get; set; }
+
+        public string ItemOwnerId { get; set; }
+
+        public User ItemOwner { get; set; }
+
+        public ActivityItemDetails ItemDetails { get; set; }
+
+        public string OwnerId { get; set; }
+
+        public User Owner { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+    }
+
+    public class ActivityItemDetails
+    {
+        public string IdeaId { get; set; }
+
+        public string TargetOwnerId { get; set; }
+
+        public bool IsUpVote { get; set; }
+
+        public User TargetOwner { get; set; }
+    }
+}
